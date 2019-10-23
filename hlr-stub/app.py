68e9e5b7d27cb7d9f85e43dc0eacb0b7805446d5
joelferreira94 <56913606+joelferreira94@users.noreply.github.com>
2019-10-23 20:51:53 +1100

    adding 664 for HLR

--- hlr-stub/app.py
+++ hlr-stub/app.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
+import uuid
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
-@app.route("/resourceFunction/msisdn", methods=['GET'])
-def number():
+@app.route("/resourceFunction/msisdn/<msisdn>", methods=['GET'])
+def number(msisdn):
     uuid_generated = uuid.uuid4()
+    number_1 = msisdn
+
     data = jsonify(
-        data = "UUID Value",
+        id = f"localhost:5000/resourceFunction/msisdn/{number_1}",
         uuid = uuid_generated,
+        features = {"name":"joel"}
     )
     return data
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5002)
